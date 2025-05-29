f6fa1692e1d111c832a2688ae50a1aa12942083e
Ashraful Sifat <ashrafulsifat26@gmail.com>
2025-05-29 19:50:31 +0600

    feat: enhance admin retrieval with flexible search and filtering capabilities

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,25 +1,36 @@
-import { PrismaClient } from "../../../../generated/prisma"
+import { Prisma, PrismaClient } from "../../../../generated/prisma"
 
 const prisma = new PrismaClient()
 
 const getAllAdmins = async (params: any) => {
+  const { searchTerm, ...filterData } = params
+  const andConditions: Prisma.AdminWhereInput[] = []
+  const adminSearchableFields = ["name", "email"]
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: adminSearchableFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    })
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    })
+  }
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
   const admins = await prisma.admin.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+    where: whereConditions,
   })
   return admins
 }
