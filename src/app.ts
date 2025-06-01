4a28b969782fe48be7fc3846ece77aff6e3ec5a9
Ashraful Sifat <ashrafulsifat26@gmail.com>
2025-06-01 07:41:23 +0600

    feat: add centralized routing for user and admin modules

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express"
 import cors from "cors"
 import { UserRoutes } from "./app/modules/User/user.route"
 import { AdminRoutes } from "./app/modules/Admin/admin.route"
+import router from "./app/routes"
 
 const app: Application = express()
 app.use(cors())
@@ -15,7 +16,6 @@ app.get("/", (req: Request, res: Response) => {
   })
 })
 
-app.use("/api/v1/users", UserRoutes)
-app.use("/api/v1/admins", AdminRoutes)
+app.use("/api/v1", router)
 
 export default app
---
+++ src/app/routes/index.ts
@@ -0,0 +1,21 @@
+import express from "express"
+import { UserRoutes } from "../modules/User/user.route"
+import { AdminRoutes } from "../modules/Admin/admin.route"
+const router = express.Router()
+
+const moduleRoutes = [
+  {
+    path: "/users",
+    route: UserRoutes,
+  },
+  {
+    path: "/admins",
+    route: AdminRoutes,
+  },
+]
+
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.route)
+})
+
+export default router
