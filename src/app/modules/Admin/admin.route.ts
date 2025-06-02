3f084f3b73ae96d4cfea279cddbcaf42979e8f9c
Ashraful Sifat <ashrafulsifat26@gmail.com>
2025-06-02 18:45:10 +0600

    feat: enhance request validation by implementing schema-based validation for updateAdminById route

--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -1,15 +1,24 @@
 import express, { Request, Response, NextFunction } from "express"
+import { AnyZodObject, z } from "zod"
 import { AdminController } from "./admin.controller"
 const router = express.Router()
 
-const validateRequest = (req: Request, res: Response, next: NextFunction) => {
-  console.log("Request validation logic here")
-  next()
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    console.log("Request validation logic here")
+    next()
+  }
 }
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+})
 
 router.get("/", AdminController.getAllAdmins)
 router.get("/:id", AdminController.getAdminById)
-router.patch("/:id", validateRequest, AdminController.updateAdminById)
+router.patch("/:id", validateRequest(update), AdminController.updateAdminById)
 router.delete("/:id", AdminController.deleteAdmin)
 router.patch("/soft-delete/:id", AdminController.softDeleteAdmin)
 
