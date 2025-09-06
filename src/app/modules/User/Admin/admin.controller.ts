6d17fac9ff5833bcfa1183daef92136d9d805a02
Md. Ashraful Alam Sifat <56783196+sifat2626@users.noreply.github.com>
2025-09-06 16:21:44 +0600

    feat: enhance getAllAdmins service to support search functionality with query parameters

--- src/app/modules/User/Admin/admin.controller.ts
+++ src/app/modules/User/Admin/admin.controller.ts
@@ -2,12 +2,20 @@ import express, { Request, Response } from "express"
 import { AdminService } from "./admin.service"
 
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await AdminService.getAllAdmins()
-  res.status(200).json({
-    message: "Admins retrieved successfully",
-    success: true,
-    data: result,
-  })
+  try {
+    const result = await AdminService.getAllAdmins(req.query)
+    res.status(200).json({
+      message: "Admins retrieved successfully",
+      success: true,
+      data: result,
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      message: "Error retrieving admins",
+      success: false,
+      error: error.message,
+    })
+  }
 }
 
 export const AdminController = {
--- src/app/modules/User/Admin/admin.service.ts
+++ src/app/modules/User/Admin/admin.service.ts
@@ -1,8 +1,40 @@
-import { PrismaClient } from "../../../../../generated/prisma"
+import { Prisma, PrismaClient } from "../../../../../generated/prisma"
 const prisma = new PrismaClient()
 
-const getAllAdmins = async () => {
-  const result = await prisma.admin.findMany()
+const getAllAdmins = async (params: any) => {
+  const andConditions: Prisma.AdminWhereInput[] = []
+  const adminSearchableFields = ["name", "email"]
+
+  // [
+  //       {
+  //         name: {
+  //           contains: params.searchTerm,
+  //           mode: "insensitive",
+  //         },
+  //       },
+  //       {
+  //         email: {
+  //           contains: params.searchTerm,
+  //           mode: "insensitive",
+  //         },
+  //       },
+  //     ],
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: adminSearchableFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    })
+  }
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+  })
   return result
 }
 
