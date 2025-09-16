3cf2f3da2260d4ab50c750be2a7677f22c8d729e
Md. Ashraful Alam Sifat <56783196+sifat2626@users.noreply.github.com>
2025-09-16 14:45:44 +0600

    feat: refactor file upload handling by integrating fileUploader utility and removing multer configuration from user routes

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,25 +1,14 @@
 import express from "express"
 import { userController } from "./user.controller"
-import multer from "multer"
 import { UserRole } from "../../../../generated/prisma"
 import { auth } from "../../middlewares/auth"
+import { fileUploader } from "../../utils/fileUploader"
 
 const router = express.Router()
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "/tmp/my-uploads")
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
-    cb(null, file.fieldname + "-" + uniqueSuffix)
-  },
-})
-
-const upload = multer({ storage: storage })
-
 router.post(
   "/create-admin",
+  fileUploader.upload.single("file"),
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   userController.createAdmin
 )
